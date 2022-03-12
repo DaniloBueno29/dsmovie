3d46bf361206b3fa54b81f6fbc7a3965736b7e2e
danilobueno29 <danilo.mb123@gmail.com>
2022-03-12 13:06:11 -0300

    add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-  <h1>Principal Page</h1>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+ export function Navbar() {
+    return(
+        <header>
+    <nav className='container'>
+      <div className='dsmovie-nav-content'>
+        <h1>DSMovie</h1>
+        <a href="https://github.com/DaniloBueno29">
+            <div className='dsmovie-contact-container'>
+                <GithubIcon />
+                <p className='dsmovie-contact-link'>/DaniloBueno29</p>
+            </div>
+        </a>
+      </div>
+    </nav>
+  </header>
+    );
+}
